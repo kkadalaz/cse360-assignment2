f4619328b04f99d8a3b9b3584aa6da5af8bf5269
Kesav Kadalazhi <kkadalaz@asu.edu>
2020-02-26 19:14:53 -0700

    Fix formatting

--- assign2/src/cse360assign2/SimpleList.java
+++ assign2/src/cse360assign2/SimpleList.java
@@ -9,108 +9,104 @@
 package cse360assign2;
 
 /**
- * this is the SimpleList class
- * it holds 10 integers
+ * this is the SimpleList class it holds 10 integers
  * 
  * @author Kesav Kadalazhi
  *
  */
 public class SimpleList {
 
-    private int[] list;
-    private int count;
-    private int LENGTH;
+	private int[] list;
+	private int count;
+	private int LENGTH;
 
-    /**
-     * instantiates the list
-     */
-    public SimpleList() {
-    	LENGTH = 10;
-        list = new int[LENGTH];
-    }
+	/**
+	 * instantiates the list
+	 */
+	public SimpleList() {
+		LENGTH = 10;
+		list = new int[LENGTH];
+	}
 
-    /**
-     * adds the element to the start of the list
-     * pushes all other elements down
-     * 
-     * @param element
-     */
-    public void add(int element) {
-    	for (int counter = LENGTH - 1; counter > 0; counter--) {
-    		list[counter] = list[counter - 1];
-    	}
-    	list[0] = element;
+	/**
+	 * adds the element to the start of the list pushes all other elements down
+	 * 
+	 * @param element
+	 */
+	public void add(int element) {
+		for (int counter = LENGTH - 1; counter > 0; counter--) {
+			list[counter] = list[counter - 1];
+		}
+		list[0] = element;
 
-    	// if list is not yet full
-    	if (count < LENGTH) {
-        	// update count
-    		count++;
-    	}
-    }
+		// if list is not yet full
+		if (count < LENGTH) {
+			// update count
+			count++;
+		}
+	}
 
-    /**
-     * removes an element from the last
-     * moves all following elements up
-     * 
-     * @param element
-     */
-    public void remove(int element) {
-    	int location = search(element);
-        if (location != -1) {
-        	for (int counter = location; counter < LENGTH - 1; counter ++) {
-        		list[counter] = list[counter + 1];
-        	}
-        	list[9] = 0;
-        }
-        // if list is not already empty
-        if (count > 0) {
-        	// update count
-            count--;        	
-        }
-    }
+	/**
+	 * removes an element from the last moves all following elements up
+	 * 
+	 * @param element
+	 */
+	public void remove(int element) {
+		int location = search(element);
+		if (location != -1) {
+			for (int counter = location; counter < LENGTH - 1; counter++) {
+				list[counter] = list[counter + 1];
+			}
+			list[9] = 0;
+		}
+		// if list is not already empty
+		if (count > 0) {
+			// update count
+			count--;
+		}
+	}
 
-    /**
-     * searches for a given element in the list
-     * if not found, return -1
-     * 
-     * @param element
-     * @return location of element
-     */
-    public int search(int element) {
-    	int location = -1;
-    	boolean found = false;
-    	
-    	for (int counter = 0; counter < LENGTH; counter++) {
-    		if (!found && list[counter] == element) {
-    			found = true;
-    			location = counter;
-    		}
-    	}
-        return location;
-    }
+	/**
+	 * searches for a given element in the list if not found, return -1
+	 * 
+	 * @param element
+	 * @return location of element
+	 */
+	public int search(int element) {
+		int location = -1;
+		boolean found = false;
 
-    /**
-     * @return number of elements in the list
-     */
-    public int count() {
-        return count;
-    }
+		for (int counter = 0; counter < LENGTH; counter++) {
+			if (!found && list[counter] == element) {
+				found = true;
+				location = counter;
+			}
+		}
+		return location;
+	}
 
-    /**
-     * returns a string version of the list
-     */
-    public String toString() {
-    	String toString = "";
-    	for (int counter = 0; counter < LENGTH - 1; counter ++) {
-    		if (list[counter] != 0) {
-        		toString += list[counter];
-        		toString += " ";			
-    		}
-        }
-    	if (list[LENGTH - 1] != 0) {
-    		toString += list[LENGTH - 1];
-    	}
-        return toString.trim();
-    }
+	/**
+	 * @return number of elements in the list
+	 */
+	public int count() {
+		return count;
+	}
+
+	/**
+	 * returns a string version of the list
+	 */
+	public String toString() {
+		String toString = "";
+		for (int counter = 0; counter < LENGTH - 1; counter++) {
+			if (list[counter] != 0) {
+				toString += list[counter];
+				toString += " ";
+			}
+		}
+		if (list[LENGTH - 1] != 0) {
+			toString += list[LENGTH - 1];
+		}
+		return toString.trim();
+	}
 
 }
